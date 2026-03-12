96a0de5263cc8598b05cffd0abd94bb0e45aafb6
TadMSTR <tadmstr@github.com>
2026-03-12 17:23:43 -0400

    panel: add CloudCLI health check (port 3004)

--- config/config.js
+++ config/config.js
@@ -37,6 +37,7 @@ module.exports = {
     { label: 'Netdata (claudebox)', url: 'http://127.0.0.1:19999', link: 'https://netdata.claudebox.me' },
     { label: 'Netdata (atlas)',     url: 'http://10.10.1.9:19998', link: null },
     { label: 'cui',                 url: 'http://127.0.0.1:3001',  link: 'https://cui.claudebox.me' },
+    { label: 'CloudCLI',            url: 'http://127.0.0.1:3004',  link: 'https://cloudcli.claudebox.me' },
     { label: 'qmd',                 url: 'http://127.0.0.1:8181',  link: null },
     { label: 'NFS mount',           url: null, mountpoint: '/mnt/atlas/claudebox', link: null },
   ],
